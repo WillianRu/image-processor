5911d67b808fc82812293aff31e1f74ea4d1b448
WillianRu <correo.willianrueda@gmail.com>
2024-11-12 10:46:05 -0500

    Fix: Handle imageData and format on ImageProcessingService

--- image-processor/src/main/java/com/ikno/application/service/ImageProcessingService.java
+++ image-processor/src/main/java/com/ikno/application/service/ImageProcessingService.java
@@ -1,11 +1,25 @@
 package com.ikno.application.service;
 
+import org.springframework.stereotype.Service;
+
 import com.ikno.domain.model.Image;
 import com.ikno.domain.model.ImageFormat;
 import com.ikno.domain.service.ImageConverterService;
 
+@Service
 public record ImageProcessingService(ImageConverterService imageConverter ) {
-    public Image processImage(Image image, ImageFormat targetFormat) {
-        return imageConverter.convertToFormat(image, targetFormat);
+    public Image processImage(byte[] imageData, String targetFormat) {
+        ImageFormat format;
+        if ("PNG".equalsIgnoreCase(targetFormat)) {
+            format = ImageFormat.png();
+        } else if ("JPEG".equalsIgnoreCase(targetFormat)) {
+            format = ImageFormat.jpeg();
+        } else {
+            throw new IllegalArgumentException("Unsupported target format: " + targetFormat);
+        }
+
+        Image image = new Image(imageData, ImageFormat.jpeg(), "originalImage");
+
+        return imageConverter.convertToFormat(image, format);
     }
 }
